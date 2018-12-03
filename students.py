2333510dd74a686337e6173a8348654e88d47b63
John Deleon <angularjohn@gmail.com>
2018-12-03 14:24:51 +0800

    finished codes for uploading students absences to database

--- students.py
+++ students.py
@@ -1,23 +1,69 @@
 import amarissedb
 import os
 
-def search():
-    print('die')
-
 menuItems = [
-    "Search Student",
+    "Student Absences",
+    "Student Tardiness",
     "Exit"
 ]
 
-def main():
+studentAbsencesItems = [
+    'Show Absences Report',
+    'Search Student',
+    'back'
+]
+
+def absencesReport():
+    cnx = amarissedb.connect()
+    if cnx == None:
+        sys.exit(1)
+    cursor = cnx.cursor()
+    cursor.execute('SELECT * from students_absences')
+    data = cursor.fetchall()
+    for datum in data:
+        print(datum[1])
+
+def studentAbsences():
     while True:
+        i=1
         os.system('cls')
+        for item in studentAbsencesItems:
+            print('[',i,']',item)
+            i+=1
+        choice = input('Enter Number of Choice >> ')
+        if choice == '1':
+            absencesReport()
+            break
+        if choice == '2':
+            studentTardiness()
+            break
+        elif choice == 'back':
+            main()
+            break
+        else:
+            os.system('cls')
+            print('not a valid input')
+    
+
+def main():
+    os.system('cls')
+    while True:
         i=1
         for item in menuItems:
             print('[',i,']',item)
             i+=1
         choice = input('Enter Number of Choice >> ')
-        try:
+        if choice == '1':
+            studentAbsences()
+            break
+        if choice == '2':
+            studentTardiness()
+            break
+        elif choice == 'exit':
+            break
+        else:
+            os.system('cls')
+            print('not a valid input')   
+                
             
-
 main()
