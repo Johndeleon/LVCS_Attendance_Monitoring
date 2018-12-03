b7dd9bc28959453e2ebf52cd606bb95e4d36dba8
John Deleon <angularjohn@gmail.com>
2018-12-03 15:13:53 +0800

    started creating reports on reports.py

--- reports.py
+++ reports.py
@@ -12,16 +12,47 @@ studentAbsencesItems = [
     'Search Student',
     'back'
 ]
+reportSubmenu = [
+    'go back to previous menu',
+    'go back to main menu',
+    'exit'
+]
 
 def absencesReport():
     cnx = amarissedb.connect()
     if cnx == None:
         sys.exit(1)
     cursor = cnx.cursor()
-    cursor.execute('SELECT * from students_absences')
-    data = cursor.fetchall()
-    for datum in data:
-        print(datum[1])
+    cursor.execute('SELECT COUNT(DISTINCT student_id)FROM students_absences')
+    absenteePop = cursor.fetchone()
+    cursor.execute('''SELECT student_id, count(*) AS qty 
+                    FROM students_absences 
+                    GROUP BY student_id 
+                    ORDER BY qty DESC 
+                    LIMIT 1;''')
+    mode = cursor.fetchone()
+    cursor.execute('SELECT full_name FROM absent_students WHERE id = %s',(mode[0],))
+    fullName = cursor.fetchone();
+    os.system('cls')
+    print('Total Number of Students with Absences: ',absenteePop[0])
+    print('Student with Most Number of Absences: ',fullName[0])
+    i=1
+    while True:
+        for item in reportSubmenu:
+            print('[',i,']',item)
+            i+=1
+        choice = input('Enter Number of Choice >> ')
+        if choice == '1':
+            studentAbsences()
+            break
+        if choice == '2':
+            main()
+            break
+        elif choice == '3':
+            break
+        else:
+            os.system('cls')
+            print('not a valid input')
 
 def studentAbsences():
     while True:
@@ -37,7 +68,7 @@ def studentAbsences():
         if choice == '2':
             studentTardiness()
             break
-        elif choice == 'back':
+        elif choice == '3':
             main()
             break
         else:
@@ -59,7 +90,7 @@ def main():
         if choice == '2':
             studentTardiness()
             break
-        elif choice == 'exit':
+        elif choice == '3':
             break
         else:
             os.system('cls')
