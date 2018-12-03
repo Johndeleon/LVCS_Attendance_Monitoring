562104c61204c6e7d45e6a4d2776898de74d1a03
John Deleon <angularjohn@gmail.com>
2018-12-03 12:41:49 +0800

    finished codes for inserting students absences to table

--- students.py
+++ students.py
@@ -1,16 +1,23 @@
 import amarissedb
 import os
 
+def search():
+    print('die')
+
 menuItems = [
-    { "Search Student": bar },
-    { "Exit": exit },
+    "Search Student",
+    "Exit"
 ]
 
 def main():
     while True:
         os.system('cls')
-        for option in menuItems:
-            print(option)
-        
+        i=1
+        for item in menuItems:
+            print('[',i,']',item)
+            i+=1
+        choice = input('Enter Number of Choice >> ')
+        try:
+            
 
 main()
